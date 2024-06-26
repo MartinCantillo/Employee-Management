e06ffb941a07be7e9bff14c9957d3a11c6370e26
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-06-26 14:29:37 -0500

    "Added constructor, properties, and data annotations to Departamento class"

--- MvcProject/Models/Departamento.cs
+++ MvcProject/Models/Departamento.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,21 @@ namespace MvcProject.Models
 {
     public class Departamento
     {
-        
+        public Departamento(int Id, string NombreD, string AreaD)
+        {
+            this.Id = Id;
+            this.NombreD = NombreD;
+            this.AreaD = AreaD;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string NombreD { get; set; }
+
+        public string AreaD { get; set; }
+
+
     }
 }
