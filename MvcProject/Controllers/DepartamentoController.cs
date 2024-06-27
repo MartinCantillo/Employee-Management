ae75fcc2d2cc24081ff7ddf68d48ffec64a2df67
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-06-27 11:23:11 -0500

    "Added Departamento model, DepartamentoController with GuardarD and Confirmacion actions, and RegistroD view. Updated AssemblyInfo.cs and various binary files."

--- MvcProject/Controllers/DepartamentoController.cs
+++ MvcProject/Controllers/DepartamentoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MvcProject.Models;
 
 namespace mvcproject.Controllers
 {
@@ -36,11 +37,28 @@ namespace mvcproject.Controllers
         {
             return View();
         }
-          public IActionResult EmpleadoxD()
+        public IActionResult EmpleadoxD()
         {
             return View();
         }
-           public IActionResult DmayorDiez()
+        public IActionResult DmayorDiez()
+        {
+            return View();
+        }
+
+
+         [HttpPost]
+        public IActionResult GuardarD(Departamento d)
+        {
+            if (ModelState.IsValid)
+            {
+
+                return RedirectToAction("Confirmacion");
+            }
+            return View(d);
+        }
+
+        public IActionResult Confirmacion()
         {
             return View();
         }
--- MvcProject/Models/Departamento.cs
+++ MvcProject/Models/Departamento.cs
@@ -9,6 +9,9 @@ namespace MvcProject.Models
 {
     public class Departamento
     {
+        public Departamento()
+        {
+        }
         public Departamento(int Id, string NombreD, string AreaD)
         {
             this.Id = Id;
@@ -18,9 +21,14 @@ namespace MvcProject.Models
 
 
         public int Id { get; set; }
-
+    
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         public string NombreD { get; set; }
 
+
+
+
+        [Required(ErrorMessage = "El área es obligatoria")]
         public string AreaD { get; set; }
 
         public ICollection<Empleado> Empleados;
