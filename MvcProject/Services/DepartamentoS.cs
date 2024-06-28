991ed572a82462cb057da71e607c74fef08af806
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-06-28 10:38:09 -0500

    "Implemented DeleteD and GetD methods, and modified SaveD method in DepartamentoS class"

--- MvcProject/Services/DepartamentoS.cs
+++ MvcProject/Services/DepartamentoS.cs
@@ -3,25 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using mvcproject.Repositories;
+using MvcProject.Data;
 using MvcProject.Models;
 
 namespace mvcproject.Services
 {
     public class DepartamentoS : IDepartamento
     {
-        public void DeleteD(int id)
+        private readonly DataContext _DataContext;
+        public DepartamentoS(DataContext _DataContext)
         {
-            throw new NotImplementedException();
+            this._DataContext = _DataContext;
         }
 
-        public ICollection<Departamento> GetD()
+        public void DeleteD(int id)
         {
-            throw new NotImplementedException();
+            if (id == 0)
+            {
+                throw new Exception("Por favor verifica");
+            }
+            else
+            {
+                var found = this._DataContext._Departamento.FirstOrDefault(d => d.Id == id);
+                if (found != null)
+                {
+
+                    this._DataContext._Departamento.Remove(found);
+                    this._DataContext.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("No encontrado");
+                }
+            }
         }
+
+        public ICollection<Departamento> GetD() => this._DataContext._Departamento.ToList();
 
         public void SaveD(Departamento d)
         {
-            throw new NotImplementedException();
+            if (d.NombreD == "" || d.AreaD == "")
+            {
+                throw new Exception("Por favor verifica");
+            }
+            else
+            {
+                this._DataContext._Departamento.Add(d);
+                this._DataContext.SaveChanges();
+            }
         }
     }
 }
